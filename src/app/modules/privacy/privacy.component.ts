2ec1f4af050eb385f5c4950590af8ed2c3461ff2
kevin <kuttihs@gmail.com>
2020-09-22 12:29:05 +0100

    My commit

--- src/app/modules/privacy/privacy.component.ts
+++ src/app/modules/privacy/privacy.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-about',
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  selector: 'app-privacy',
+  templateUrl: './privacy.component.html',
+  styleUrls: ['./privacy.component.css']
 })
 export class AboutComponent implements OnInit {
 
