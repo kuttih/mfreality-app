0abf3d39a7e30e345de21b509aa0c856f860f634
kevin <kuttihs@gmail.com>
2020-09-23 08:40:59 +0100

    My commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { HomeComponent } from './modules/home/home.component';
 import { DefaultComponent } from './layouts/default/default.component';
 import { AboutComponent } from './modules/about/about.component';
 import { PrivacyComponent } from './modules/privacy/privacy.component';
+import { FAQComponent } from './modules/faq/faq.component';
 import { ServicesComponent } from './modules/services/services.component';
 import { ContactComponent } from './modules/contact/contact.component';
 import { EnvironmentComponent } from './layouts/environment/environment.component';
@@ -29,6 +30,8 @@ const routes: Routes = [
     path:'',component:HomeComponent},
     {path:'about',component:AboutComponent},
     {path:'privacy',component:PrivacyComponent},
+    {path:'faq',component:FAQComponent},
+    {path:'terms',component:TermsComponent},
     {path:'services',component:ServicesComponent},
     {path:'contact',component:ContactComponent}
     
